c0ab1823560eda4851f66656257d5d2bcc8d5569
Ellie <elliemmanifold@gmail.com>
2019-11-25 16:00:48 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+set    :session_secret, "here be dragons"
+
+get '/' do
+  "hello! andrea!"
+end
+
+get '/blog' do
+  "This is the blog page"
+end
+
+get '/pricing' do
+  "This is the pricing page"
+end
+
+get '/secret' do
+  "This is the secret page"
+end
