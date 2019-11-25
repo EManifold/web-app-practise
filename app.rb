e0375e12b67d5ab458c8077750a1651fe7e6fd1f
Ellie <elliemmanifold@gmail.com>
2019-11-25 17:00:36 +0000

    Replace /cat with /random-cat and /named-cat

--- app.rb
+++ app.rb
@@ -17,7 +17,13 @@ get '/secret' do
   "This is the secret page"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
